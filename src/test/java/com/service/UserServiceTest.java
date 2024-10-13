4ecdc40b16f6e90eea689d2b3d138f12cb3a7787
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-10-14 01:37:13 +0300

    Созданы зависимости pom

---
+++ src/test/java/com/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package test.java.com.service;
+
+import main.java.com.model.User;
+import main.java.com.service.InMemoryUserService;
+import main.java.com.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+//import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+}
